626498a7c7075503f38ac906898e18d3946d0a2a
Chan <rlackswhd200@gmail.com>
2025-08-18 16:09:42 +0900

    complete

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,31 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext() {
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
+import com.ll.domain.testPost.testPost.service.TestPostService;
 
+public class ApplicationContext {
+    private TestPostService tempTestPostService;
+    private TestPostRepository testPostRepository;
+    private TestFacadePostService TestFacadePostService;
+    public ApplicationContext() {
+        this.testPostRepository = new TestPostRepository();
+        this.tempTestPostService = new TestPostService(testPostRepository);
+        this.TestFacadePostService = new TestFacadePostService(tempTestPostService, testPostRepository);
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        if (beanName.equals("testPostService")) {
+            TestPostRepository repo = genBean("testPostRepository");
+            return (T) tempTestPostService;
+        }
+        else if( beanName.equals("testPostRepository")) {
+            return (T) testPostRepository;
+        }
+
+        else if ( beanName.equals("testFacadePostService")){
+            return (T) TestFacadePostService;
+        }
+        return null;
     }
 }
