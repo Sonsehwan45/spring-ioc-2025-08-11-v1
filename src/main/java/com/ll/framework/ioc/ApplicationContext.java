b1f8d6d0fb85fab0c865d2db2b3c50cd8420e168
luaerix <luaerix@naver.com>
2025-08-19 16:58:39 +0900

    FacadePostService 빈 생성

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,6 +1,7 @@
 package com.ll.framework.ioc;
 
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
 import java.util.HashMap;
@@ -14,5 +15,8 @@ public class ApplicationContext {
         TestPostRepository testPostRepository = new TestPostRepository();
 
         TestPostService testPostService = new TestPostService(testPostRepository);
+
+        TestFacadePostService testFacadePostService = new TestFacadePostService(testPostService, testPostRepository);
+
     }
 }
