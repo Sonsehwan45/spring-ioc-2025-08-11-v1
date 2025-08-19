39695b4d547eb9bbd1b7d43c7312754682d063ea
luaerix <luaerix@naver.com>
2025-08-19 16:47:22 +0900

    Repository 빈 생성

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,16 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApplicationContext {
+    private final Map<String, Object> beans = new HashMap<>();
+
     public ApplicationContext() {
 
-    }
+        TestPostRepository testPostRepository = new TestPostRepository();
 
-    public <T> T genBean(String beanName) {
-        return (T) null;
     }
 }
