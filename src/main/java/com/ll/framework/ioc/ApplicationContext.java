f4370d7fbe6c9996cde85128d7000820d3ad64b8
luaerix <luaerix@naver.com>
2025-08-19 16:53:57 +0900

    Service 빈 생성

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,6 +1,7 @@
 package com.ll.framework.ioc;
 
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestPostService;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,5 +13,6 @@ public class ApplicationContext {
 
         TestPostRepository testPostRepository = new TestPostRepository();
 
+        TestPostService testPostService = new TestPostService(testPostRepository);
     }
 }
